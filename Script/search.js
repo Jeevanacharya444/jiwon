8352c1d4f6cf5890c3fa0e9eac53949eee4292e9
Jeevanacharya444 <167285682+Jeevanacharya444@users.noreply.github.com>
2025-01-09 17:47:07 +0545

    Delete Script directory

--- Script/search.js
+++
@@ -1,9 +0,0 @@
-const search = [];
-
-function search(){
-const inputElement = document.querySelector('.js-name');
-  const name = inputElement.value;
-  search.push(name);
-  console.log(search);
-  inputElement.value = '';
-}
